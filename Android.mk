60a864085916e2cea61de9ecfb009746987cced1
Ameya Thakur <ameyat@codeaurora.org>
2016-09-23 14:50:14 -0700

    bootcontrol: Only compile when the A/B feature is enabled
    We now only compile the HAL when the A/B feature flag is set.
    
    Change-Id: I822fe4579de86e8785dd5708e4a063311a61d060

--- Android.mk
+++ Android.mk
@@ -1,6 +1,8 @@
 # Preset TARGET_USES_HARDWARE_QCOM_BOOTCTRL for existing platforms.
 ifneq ($(filter msm8996 msm8998 sdm710,$(TARGET_BOARD_PLATFORM)),)
+ifeq ($(AB_OTA_UPDATER),true)
 TARGET_USES_HARDWARE_QCOM_BOOTCTRL := true
+endif
 endif
 
 ifeq ($(strip $(TARGET_USES_HARDWARE_QCOM_BOOTCTRL)),true)
